ce70b402c90490541538b23ee64b33ef6a974791
Brian J. Watson <watson@insidevault.com>
2015-03-22 10:47:19 -0700

    Add comment. Improve a line break.

--- plot3.R
+++ plot3.R
@@ -5,6 +5,7 @@ NEI <- readRDS("summarySCC_PM25.rds")
 NEI$fips <- factor(NEI$fips)
 NEI$SCC <- factor(NEI$SCC)
 NEI$Pollutant <- factor(NEI$Pollutant)
+# Define order for types, to control the faceted plot order
 NEI$type <- factor(NEI$type, levels = c("POINT", "NONPOINT", "ON-ROAD", "NON-ROAD"))
 NEI$year <- factor(NEI$year)
 
@@ -13,8 +14,9 @@ emissionsByYear <- aggregate(Emissions ~ year + type, NEI[NEI$fips == "24510",],
 
 # Generate faceted plot by type
 png("plot3.png", height = 960)
-p <- qplot(year, Emissions, data = emissionsByYear, facets = type ~ ., geom = "bar", stat = "identity", 
-      main = "Total PM2.5 Emissions by Type in Baltimore City, Maryland", 
-      xlab = "Year", ylab = "PM2.5 Emissions (Tons)")
+p <- qplot(year, Emissions, data = emissionsByYear, facets = type ~ ., 
+           geom = "bar", stat = "identity", 
+           main = "Total PM2.5 Emissions by Type in Baltimore City, Maryland", 
+           xlab = "Year", ylab = "PM2.5 Emissions (Tons)")
 print(p)
 dev.off()
